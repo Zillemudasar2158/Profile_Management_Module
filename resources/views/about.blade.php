f71f3b7c6cb83538b249e4caac9055815ead4453
Zillemudasar2158 <zillemudasar60@gmail.com>
2025-05-21 22:00:37 +0500

    setting update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,7 @@
   <div class="row content">
     <div class="col-sm-1"></div>    
     <div class="col-sm-10 text-left"> 
-      <h1 style="color: #219c9c">Summary d</h1>
+      <h1 style="color: #219c9c">Summary</h1>
       <p style="text-indent: 50px">Passionate and detail-oriented Laravel Developer with 3 years of experience in building web applications using PHP, Laravel, and modern frontend technologies. Skilled in developing scalable APIs, implementing MVC architecture, and optimizing database queries using Eloquent ORM. Proficient in integrating third-party services, deploying on cloud platforms, and maintaining high standards of code quality through testing and debugging.</p>
       <hr>
       <h4>
